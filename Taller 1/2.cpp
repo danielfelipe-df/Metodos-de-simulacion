dce9ef742848dc8c51d400e8550ce5c3053117df
Daniel Ortiz <dfortizc@unal.edu.co>
2019-04-28 18:08:48 -0500

    Se añade el método de bisección en el archivo 2.cpp

--- Taller 1/2.cpp
+++ Taller 1/2.cpp
@@ -7,14 +7,14 @@ const double ERR =1e-7;
 double f1(double r1, double r2, double r, double lambda);
 double f2(double r1, double r2, double r, double lambda);
 void UnPasoDeRungeKutta4(double &r1, double &r2, double &r, double &dr, double &lambda);
-double Biseccion(double a, double b);
-double f(double a);
+double Biseccion(int &a, int &b, double *f);
+
 
 int main(void)
 {
   double r1 = 1, r2 = 0, r;
   double dr = 0.001;
-  double lambda = 2.4048, dlambda;
+  double lambda = 2.4048, dlambda, lambda_max;
 
   //-----Punto a-----
 
@@ -31,7 +31,7 @@ int main(void)
 
   //-----Punto b-----
 
-  dlambda = 0.01;
+  dlambda = 0.001;
   /*
   std::cout << "set terminal gif animate" << std::endl;
   std::cout << "set output 'pelicula1.gif'" << std::endl;
@@ -53,8 +53,35 @@ int main(void)
   std::cout << "pause 10" << std::endl;
 
   //----- Punto c------
+  dlambda = 0.001;  lambda_max = 15;
+  //Este N se define de esta forma porque lo que hago es generar un array que tenga el tamaño necesario para guardar los datos
+  int N = (int)lambda_max/dlambda;
+  double data[N];
+  int i;
+  for(lambda=dlambda, i=0; lambda<lambda_max; lambda+=dlambda, i++){
+    r1 = 1;   r2 = 0;
+    for(r=dr/10; r<1; r+=dr){
+      UnPasoDeRungeKutta4(r1,r2,r,dr,lambda);
+    }
+    //Aquí se guarda cada dato de la función en un array
+    data[i] = r1;
+  }
 
-
+  std::cout << N << std::endl;
+  //Las variables int son las que me indican la posición que voy a mirar en el array
+  //En el caso de 'a' estoy interesado que casilla ocupa el valor lambda=3 en el array. Y en el caso de 'b' es lambda=6
+  double algo;   int a = 3/dlambda, b=6/dlambda;
+  algo = Biseccion(a,b,data);
+  //Aquí imprimo, inicialmente, el número de la casilla, y después el valor de lambda al que corresponde
+  std::cout << algo << " " << algo*dlambda << std::endl;
+  int cosa = (int)algo;
+  //Aquí imprimo el valor del cero
+  std::cout << data[cosa] << std::endl;
+  //Aquí grafico en donde quedaría ubicado el cero de la función
+  std::cout << "set parametric" << std::endl;
+  std::cout << "set trange[-0.6:1]" << std::endl;
+  std::cout << "plot " << algo*dlambda << ",t , 'datos_2b.dat' w l , t*10,0" << std::endl;
+  std::cout << "pause 5" << std::endl;
 
   //----- Punto d------
 
@@ -80,18 +107,27 @@ void UnPasoDeRungeKutta4(double &r1, double &r2, double &r, double &dr, double &
   r += dr;  r1 += dr*(dr11+2*(dr12+dr13)+dr14)/6;          r2 += dr*(dr21+2*(dr22+dr23)+dr24)/6;
 }
 
-double Biseccion(double a, double b)
+//Aquí le paso como argumentos las posiciones inciales en el array para hacer el método.
+//Después le paso la dirección (tipo puntero) de la primera casilla del array.
+double Biseccion(int &a, int &b, double *f)
 {
-  double m,fa,fm;
-  fa=f(a);
-  while(b-a>ERR){
-    m=(a+b)/2; fm=f(m); //calculo m;
-    if(fa*fm<0)  //  if(f(a) y f(m) son de signo contrario)
+  //Defino la variable 'm', la cual será mi variable de intercambio en el método. (Nótese que representará el número de una casilla).
+  //Los punteros fa y fm serán las direcciones de la posición 'a' y 'm'
+  int m;     double *fa, *fm;
+  //Aquí se hace álgebra de punteros para asignarle la dirección a fa
+  fa = f + a;
+  //Aquí el while del método. Y lo que hago es definir que mientras la diferencia de casillas sea 5 o menor termine de buscar la raíz.
+  while(b-a > 5){
+    //Asignación de las variables
+    m=(int)(a+b)/2;  fm = f + m;
+    //Aplicación del método de bisección
+    if((*fa)*(*fm)<0){
       b=m;
-    else
-      {a=m; fa=fm;}
+    }
+    else{
+      a=m;  fa=fm;
+    }
   }
-  return (a+b)/2;
+  //Devuelvo la ubicación de la raíz.
+  return (a+b)/2.0;
 }
-
-double f(double a){return a;}
--- Taller 1/4.cpp
+++ Taller 1/4.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
 #include <cmath>
 
-int int main(void)
+void Iniciar();
+
+class Pendulo
 {
+private:
+  double r, theta, l, x0;
+
+public:
+  void First_ubication(double x0, double r0, double theta0, double l0);
+  void Dibujarse();
+};
+
+
+int main(void)
+{
+  Pendulo objeto[3];
+  double x = 3, r0 = 1, theta0 = M_PI/2, l0 = 2;
+  Iniciar();
+  for(int i=0, x=0; i<4; x+=2*r0, i++){
+    objeto[i].First_ubication(x, r0, theta0, l0);
+    objeto[i].Dibujarse();
+  }
+  std::cout << std::endl;
+  std::cout << "pause 10" << std::endl;
   return 0;
+}
+
+//------- Funciones de la clase Pendulo --------
+void Pendulo::First_ubication(double x, double r0, double theta0, double l0)
+{
+  r = r0;  theta = theta0;  x0 = x;   l = l0;
+}
+
+void Pendulo::Dibujarse()
+{
+  std::cout << " , " << r << "*cos(t) + " << x0 << " + " << l << "*sin(" << theta << ")," << r << "*sin(t) + " << l << "*(1+cos(" << theta << "))";
+  std::cout << " , " << x0 << "," << l/10 << "*t + " << r;
+}
+
+//------ Funciones normales ------
+void Iniciar()
+{
+  std::cout << "set xrange[-10:10]" << std::endl;
+  std::cout << "set yrange[-10:10]" << std::endl;
+  std::cout << "set parametric" << std::endl;
+  std::cout << "set trange[0:10]" << std::endl;
+  std::cout << "plot 0,0";
 }
