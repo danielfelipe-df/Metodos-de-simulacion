675c06ef7ab9b05110d500fd7d05bceb67631906
acmoral <36461052+acmoral@users.noreply.github.com>
2019-04-28 19:54:04 -0500

    al final no tenia tantos cambios

--- 1/2a.cpp
+++ 1/2a.cpp
@@ -4,27 +4,27 @@
 #include <fstream>
 #include"rk4.h"
 //constantes del problema//
-const lambda =1;
-const double h=0.1;//tamaño de paso
-const double tf=1000;//tiempo final
+const double lambda =1;
+const double h=0.001;//tamaño de paso
+const double tf=10;//tiempo final
 const int size =2;//cuantas variables son independientes, y & R
 
 int main(void)
 {
-  std::ofstream file("data.dat");
+  
   std::vector<double> y(size);
   //condiciones iniciales
   y[0]=1;//R(0)
   y[1]=0;//y(0), y=R'
+  std::ofstream file("2a.dat");
+   std::cout<<"set xrange[0:10]"<<std::endl;
+   std::cout<<"set yrange[-0.6:1]"<<std::endl;
   for(double r=0.01;r<tf;r+=h)
-    { fout1 << r << " " << y[0]<<'\n';
+    {  file<<r<<" "<<y[0]<<std::endl;
       rk4(r,h,y,lambda);
       }
-  file.close();
-  std::cout << "set xrange[0.01:10]" << std::endl;
-  std::cout << "set yrange[-1:1]" << std::endl;
-  std::cout << "plot 'data.dat' using 1:2 w l title 'R(r)' " << std::endl;
-  std::cout << "pause 10" << std::endl;
+  std::cout<<"plot \"2a.dat\" w lines,'datos_2a.dat' w l "<<std::endl;
+   std::cout << "pause 10" << std::endl;
   
   return 0;
   
