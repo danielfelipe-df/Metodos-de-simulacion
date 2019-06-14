ce30f1b64d257cdb57f613d53b621c4b2a6f377d
andrea <andsteinmora@gmail.com>
2019-06-14 18:09:53 -0500

    añado modificaciones

--- Taller 2/punto1.cpp
+++ Taller 2/punto1.cpp
@@ -7,14 +7,14 @@ using namespace std;
   
 const int Lx=256; 
 const int Ly=256;
-const double p0=0.25;
+const double p0=0.5;
 const double p=0.25;
 const double p1=1-(2*p0)-p;
 const double p2=p;
 class LatticeGas{
 private:
-  int V[4][1];
-  int n[Lx][Ly][4], nnew[Lx][Ly][4];//4 direcciones arriba abajo derecha izquierda
+  int V[4][2];
+  int n[Lx][Ly][4], nnew[Lx][Ly][4];//4 direcciones derecha 0 , arriba 1, izquierda 2, abajo 3
 public:
   void Inicie(int B, double mu, double sigma, Crandom & ran64);
   void Show(bool ImprimirNew);
@@ -37,7 +37,7 @@ double LatticeGas::GetSigma2(void)
   for(xprom=0, i=0; i<Lx; i++){
     for(j=0; j<Ly; j++){
       for(k=0; k<4; k++){
-        xprom+=i*j*n[i][j][k];
+        xprom+=pow((i*i)+(j*j),0.5)*n[i][j][k];
       }
     }
   }
@@ -46,7 +46,7 @@ double LatticeGas::GetSigma2(void)
   for(sigma2=0, i=0; i<Lx; i++){
     for(j=0; j<Ly; j++){
       for(k=0; k<4; k++){
-        sigma2+=i*i*j*j*n[i][j][k];
+        sigma2+=((i*i)+(j*j))*n[i][j][k];
       }
     }
   }
@@ -55,33 +55,34 @@ double LatticeGas::GetSigma2(void)
 
   return sigma2;
 }
-void LatticeGas::Adveccione(void){
+  void LatticeGas::Adveccione(void){
   for(int i=0;i<Lx;i++)//para cada celda
-    for(j=0; j<Ly; j++)
+    for(int j=0; j<Ly; j++)
       for(int k=0;k<4;k++)
         n[(i+Lx+V[k][0])%Lx][(i+Ly+V[k][1])%Ly][k]=nnew[i][j][k]; // el primer parentesis con modulo genera fronteras periodicas
 }  
  
 void LatticeGas::Colisione(Crandom & ran64){
-  for(int i=0;i<Lx;i++)
-    for(int j=0; j<Ly; j++)//ir celda por celda
+  for(int i=0;i<Lx;i++){
+    for(int j=0; j<Ly; j++){//ir celda por celda
       if(ran64.r()<p0){
         for(int k=0;k<4;k++)
           nnew[i][j][k]=n[i][j][k];//se deja igual
       }
-      else if(ran64.r()<(p0+p)){
+      else if(ran64.r()<(p0+p) && ran64.r()>p0 ){
         for(int k=0;k<4;k++)
-          nnew[(1+i)%4][(1+j)%4][k]=n[i][j][k];//90 grados
+	  nnew[i][j][(1+k)%4]=n[i][j][k];//90 grados
       }
-      else if(ran64.r()<(p0+p+p1)){
+      else if(ran64.r()<(p0+p+p1)&&ran64.r()>(p0+p) ){
         for(int k=0;k<4;k++)
-          nnew[(2+i)%4][2+j)%4][k]=n[i][j][k];//180 grados
+          nnew[i][j][(2+k)%4]=n[i][j][k];//180 grados
       }
       else{
         for(int k=0;k<4;k++)
-          nnew[(3+i)%4][(3+j)%4][k]=n[i][j][k];//270 grados
+          nnew[i][j][(3+k)%4]=n[i][j][k];//270 grados
       }
- 
+    }
+  }
 }
 
 void LatticeGas::Show(bool ImprimirNew){
@@ -93,19 +94,23 @@ void LatticeGas::Show(bool ImprimirNew){
   }
 }
 void LatticeGas::Inicie(int B, double mu, double sigma, Crandom & ran64){
-  V[0][0]=1;   V[1][0]=1; V[2][0]=0;  V[3][0]=0;
-  V[0][1]=0;   V[1][1]=0; V[2][1]=1;  V[3][1]=-1;
+  V[0][0]=1;   V[1][0]=0; V[2][0]=-1;  V[3][0]=0;
+  V[0][1]=0;   V[1][1]=1; V[2][1]=0;  V[3][1]=-1;
   //iniciar los contenidos
   int i,k,b;
-  for(i=0;i<Lx;i++)
-    for(k=0;k<4;k++)
-      n[i][k]=nnew[i][k]=0;
+  for(i=0;i<Lx;i++){
+    for(int j=0;j<Ly;j++){
+      for(k=0;k<4;k++)
+	n[i][j][k]=nnew[i][j][k]=0;
   while(B>0){ // colocar B bolitas
     i=(int)ran64.gauss(mu,sigma); if(i<0) i=0; if(i>Lx-1) i=Lx-1; // genera un numero que no se sale de la celda
     k=(int)4*ran64.r(); // genera un mumero entre 0 y 3
-
-    if(n[i][k]==0){
-      n[i][k]=1;B--; // reduce a B
+    
+    if(n[i][j][k]==0){
+      n[i][j][k]=1;B--; // reduce a B
+    }
+  }
+      
     }
   }
 }
@@ -113,7 +118,7 @@ void LatticeGas::Inicie(int B, double mu, double sigma, Crandom & ran64){
 int main(void){
   LatticeGas Difusion;
   Crandom ran64(1);
-  int B=400; double mu=Lx/2.0,sigma=16;
+  int B=600; double mu=Lx/2.0,sigma=16;
   int t, tmax=350;
   Difusion.Inicie(B,mu,sigma,ran64);
   // Difusion.Show(false);
