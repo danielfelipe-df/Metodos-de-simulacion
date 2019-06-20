131ad55f810c007057d74e8c6c9f096acd0243fd
saguileran <saguileran@unal.edu.co>
2019-06-20 01:15:59 -0500

    Comentando el codigo pero falta punto 5 y 6

--- Taller 2/LeyDeSnell.cpp
+++ Taller 2/LeyDeSnell.cpp
@@ -27,17 +27,15 @@ public:
   double rho(int ix,int iy,bool UseNew);
   double Jx(int ix,int iy,bool UseNew);
   double Jy(int ix,int iy,bool UseNew);
-  double feq(double rho0,double Jx0,double Jy0,int i, double Ccelda);
-  double Ccelda(int ix, int iy);
-  //double AUX(double Ccelda){return 1-(1-W0)*3*Ccelda*Ccelda;};
+  double feq(double rho0,double Jx0,double Jy0,int i, double Ccelda); //Ahora se coloca Ccelda como argumento para calcular siempre C, para cada casilla
+  double Ccelda(int ix, int iy);  //Se define la función que calcula C para cada celda
   void Colisione(void);
   void Adveccione(void);
   void Inicie(double rho0,double Jx0,double Jy0);
   void ImponerCampos(int t);
   void Imprimase(const char * NombreArchivo);
-  void Imprimir(int t, int ix, int iy, const char * NombreArchivo);
 };
-//LatticeBoltzmann::Casilla(int ix, int iy){return ix+iy;}
+
 LatticeBoltzmann::LatticeBoltzmann(void){
   //Cargar los pesos
   w[0]=W0; w[1]=w[2]=w[3]=w[4]=(1-W0)/4.0;
@@ -66,11 +64,11 @@ double LatticeBoltzmann::Jy(int ix,int iy,bool UseNew){
     if(UseNew) suma+=fnew[ix][iy][i]*V[1][i]; else suma+=f[ix][iy][i]*V[1][i];
   return suma;
 }
-double LatticeBoltzmann::feq(double rho0,double Jx0,double Jy0,int i, double Ccelda){
+double LatticeBoltzmann::feq(double rho0,double Jx0,double Jy0,int i, double Ccelda){ //Se agrega Ccelda como argumento de la función
   if(i==0)
-    return rho0*(1-(1-W0)*3*Ccelda*Ccelda);
+    return rho0*(1-(1-W0)*3*Ccelda*Ccelda); //AUX en forma explicita
   else
-    return w[i]*(3*Ccelda*Ccelda*rho0+3*(V[0][i]*Jx0+V[1][i]*Jy0));
+    return w[i]*(3*Ccelda*Ccelda*rho0+3*(V[0][i]*Jx0+V[1][i]*Jy0)); //Se colocan las constantes explicitamente, TresC2 y AUX0
 }
 void LatticeBoltzmann::Colisione(void){
   int ix,iy,i; double rho0,Jx0,Jy0;
@@ -80,7 +78,7 @@ void LatticeBoltzmann::Colisione(void){
       //Calcular las cantidades macroscópicas
       rho0=rho(ix,iy,false);  Jx0=Jx(ix,iy,false);  Jy0=Jy(ix,iy,false);
       for(i=0;i<Q;i++)
-        fnew[ix][iy][i]=UmUtau*f[ix][iy][i]+Utau*feq(rho0,Jx0,Jy0,i,Ccelda(ix,iy));
+        fnew[ix][iy][i]=UmUtau*f[ix][iy][i]+Utau*feq(rho0,Jx0,Jy0,i,Ccelda(ix,iy)); //Se agregua Ccelda como argumento
     }
 }
 void LatticeBoltzmann::Adveccione(void){
@@ -93,7 +91,7 @@ void LatticeBoltzmann::Inicie(double rho0,double Jx0,double Jy0){
   for(int ix=0;ix<Lx;ix++)
     for(int iy=0;iy<Ly;iy++)
       for(int i=0;i<Q;i++)
-        f[ix][iy][i]=feq(rho0,Jx0,Jy0,i,Ccelda(ix,iy));
+        f[ix][iy][i]=feq(rho0,Jx0,Jy0,i,Ccelda(ix,iy)); //Se agregua Ccelda y se calcula para todas las casillas siguiendo el suavizado definida, tanh
 }
 void LatticeBoltzmann::ImponerCampos(int t){
   int i,ix,iy; double lambda,omega,rho0,Jx0,Jy0;
@@ -101,7 +99,7 @@ void LatticeBoltzmann::ImponerCampos(int t){
   for(iy=0;iy<Ly;iy++){
    rho0=10*sin(omega*t); Jx0=Jx(ix,iy,false); Jy0=Jy(ix,iy,false);
    for(i=0;i<Q;i++){
-     fnew[ix][iy][i]=feq(rho0,Jx0,Jy0,i,Ccelda(ix,iy));}
+     fnew[ix][iy][i]=feq(rho0,Jx0,Jy0,i,Ccelda(ix,iy));} //Se agregua Ccelda como argumento y se calcula para cada celda de la condición inicial
    }
 }
 void LatticeBoltzmann::Imprimase(const char * NombreArchivo){
@@ -116,24 +114,22 @@ void LatticeBoltzmann::Imprimase(const char * NombreArchivo){
   }
   MiArchivo.close();
 }
+//Se define la función para calcular C en cada celda
 double LatticeBoltzmann::Ccelda(int ix, int iy){
  int ix0=100;
- return 0.5/(0.5*tanh(ix-ix0)+1.5);
+ return 0.5/(0.5*tanh(ix-ix0)+1.5); //Suavizado realizado, antes de ix0 n=1, después n=2
 }
-
 
 int main(void){
   LatticeBoltzmann Ondas;
   int t,tmax=1000;
 
   // Estos comandos se descomentan si se quiere guardar el gif
-  
   //std::cout << "set terminal gif animate" << std::endl;
   //std::cout << "set output 'ondas.gif'" << std::endl;
   
   //Estos comandos se descomentan para hacer el gif
-
-  cout << "set pm3d map" << endl;
+   cout << "set pm3d map" << endl;
   //cout << "set size ratio 1" << endl;
   //cout << "set palette defined (-1 \"red\", 0 \"white\", 1 \"blue\")" << endl;
   //cout << "set cbrange[-1:1]" << endl;
