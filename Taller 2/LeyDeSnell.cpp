46ac8d8258d17fa8c2ec030f2a2e2b1307193865
Daniel Ortiz <dfortizc@unal.edu.co>
2019-06-21 23:30:46 -0500

    Se añaden algunas correcciones en el archivo que envió Sebastián sobre la Ley de Snell

--- Taller 2/LeyDeSnell.cpp
+++ Taller 2/LeyDeSnell.cpp
@@ -65,20 +65,21 @@ double LatticeBoltzmann::Jy(int ix,int iy,bool UseNew){
   return suma;
 }
 double LatticeBoltzmann::feq(double rho0,double Jx0,double Jy0,int i, double Ccelda){ //Se agrega Ccelda como argumento de la función
+  double TresC2 = 3*Ccelda*Ccelda, Aux = 1-TresC2*(1-W0);
   if(i==0)
-    return rho0*(1-(1-W0)*3*Ccelda*Ccelda); //AUX en forma explicita
+    return rho0*Aux; //AUX en forma explicita
   else
-    return w[i]*(3*Ccelda*Ccelda*rho0+3*(V[0][i]*Jx0+V[1][i]*Jy0)); //Se colocan las constantes explicitamente, TresC2 y AUX0
+    return w[i]*(TresC2*rho0+3*(V[0][i]*Jx0+V[1][i]*Jy0)); //Se colocan las constantes explicitamente, TresC2 y AUX0
 }
 void LatticeBoltzmann::Colisione(void){
-  int ix,iy,i; double rho0,Jx0,Jy0;
+  int ix,iy,i; double rho0,Jx0,Jy0,c;
   //Para cada celda
   for(ix=0;ix<Lx;ix++)
     for(iy=0;iy<Ly;iy++){
       //Calcular las cantidades macroscópicas
-      rho0=rho(ix,iy,false);  Jx0=Jx(ix,iy,false);  Jy0=Jy(ix,iy,false);
+      rho0=rho(ix,iy,false);  Jx0=Jx(ix,iy,false);  Jy0=Jy(ix,iy,false);  c = Ccelda(ix,iy);
       for(i=0;i<Q;i++)
-        fnew[ix][iy][i]=UmUtau*f[ix][iy][i]+Utau*feq(rho0,Jx0,Jy0,i,Ccelda(ix,iy)); //Se agregua Ccelda como argumento
+        fnew[ix][iy][i]=UmUtau*f[ix][iy][i]+Utau*feq(rho0,Jx0,Jy0,i,c); //Se agregua Ccelda como argumento
     }
 }
 void LatticeBoltzmann::Adveccione(void){
@@ -88,19 +89,23 @@ void LatticeBoltzmann::Adveccione(void){
         f[(ix+V[0][i]+Lx)%Lx][(iy+V[1][i]+Ly)%Ly][i]=fnew[ix][iy][i];
 }
 void LatticeBoltzmann::Inicie(double rho0,double Jx0,double Jy0){
-  for(int ix=0;ix<Lx;ix++)
-    for(int iy=0;iy<Ly;iy++)
-      for(int i=0;i<Q;i++)
-        f[ix][iy][i]=feq(rho0,Jx0,Jy0,i,Ccelda(ix,iy)); //Se agregua Ccelda y se calcula para todas las casillas siguiendo el suavizado definida, tanh
+  double c; int ix,iy,i;
+  for(ix=0;ix<Lx;ix++)
+    for(iy=0;iy<Ly;iy++){
+      c = Ccelda(ix,iy);
+      for(i=0;i<Q;i++)
+        f[ix][iy][i]=feq(rho0,Jx0,Jy0,i,c); //Se agregua Ccelda y se calcula para todas las casillas siguiendo el suavizado definida, tanh
+    }
 }
 void LatticeBoltzmann::ImponerCampos(int t){
-  int i,ix,iy; double lambda,omega,rho0,Jx0,Jy0;
-  lambda=10; omega=2*M_PI*lambda; ix=0; //ix=Lx/2; iy=Ly/2;
+  int i,ix,iy; double lambda,omega,rho0,Jx0,Jy0,c;
+  lambda=20; ix=0; //ix=Lx/2; iy=Ly/2;
   for(iy=0;iy<Ly;iy++){
-   rho0=10*sin(omega*t); Jx0=Jx(ix,iy,false); Jy0=Jy(ix,iy,false);
-   for(i=0;i<Q;i++){
-     fnew[ix][iy][i]=feq(rho0,Jx0,Jy0,i,Ccelda(ix,iy));} //Se agregua Ccelda como argumento y se calcula para cada celda de la condición inicial
-   }
+    c = Ccelda(ix,iy); omega=2*M_PI*c/lambda; rho0=10*sin(omega*t); Jx0=Jx(ix,iy,false); Jy0=Jy(ix,iy,false);
+    for(i=0;i<Q;i++){
+      fnew[ix][iy][i]=feq(rho0,Jx0,Jy0,i,c);
+    } //Se agregua Ccelda como argumento y se calcula para cada celda de la condición inicial
+  }
 }
 void LatticeBoltzmann::Imprimase(const char * NombreArchivo){
   ofstream MiArchivo(NombreArchivo); double rho0;
@@ -116,24 +121,25 @@ void LatticeBoltzmann::Imprimase(const char * NombreArchivo){
 }
 //Se define la función para calcular C en cada celda
 double LatticeBoltzmann::Ccelda(int ix, int iy){
- int ix0=100;
- return 0.5/(0.5*tanh(ix-ix0)+1.5); //Suavizado realizado, antes de ix0 n=1, después n=2
+ int ix0=25; double n = 0.5*tanh(ix-ix0)+1.5;
+ return 0.5/n; //Suavizado realizado, antes de ix0 n=1, después n=2
 }
 
 int main(void){
   LatticeBoltzmann Ondas;
-  int t,tmax=10;
+  int t,tmax=100;
 
   // Estos comandos se descomentan si se quiere guardar el gif
-  //std::cout << "set terminal gif animate" << std::endl;
-  //std::cout << "set output 'ondas.gif'" << std::endl;
+  std::cout << "set terminal gif animate" << std::endl;
+  std::cout << "set output 'ondas_costado.gif'" << std::endl;
   
   //Estos comandos se descomentan para hacer el gif
    cout << "set pm3d" << endl;
-  //cout << "set size ratio 1" << endl;
-  //cout << "set palette defined (-1 \"red\", 0 \"white\", 1 \"blue\")" << endl;
-  //cout << "set cbrange[-1:1]" << endl;
-  //cout << "set xrange[0:200]; set yrange[0:200]" << endl;
+  cout << "set size ratio 1" << endl;
+  cout << "set palette defined (-5 \"red\", 0 \"white\", 5 \"blue\")" << endl;
+  cout << "set cbrange[-5:5]" << endl;
+  cout << "set xrange[0:50]; set yrange[0:200]; set zrange[-10:10]" << endl;
+  cout << "set view 90,0" << endl;
 
   Ondas.Inicie(0,0,0);
   for(t=0;t<tmax;t++){
